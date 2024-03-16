a6414bef0cf211eca30a6681d256746b6e6b55d5
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-03-16 13:03:44 +0100

    Add onMount hook to request notification permission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { requestNotificationPermission } from "$lib/utilities";
+  import { onMount } from "svelte";
 
   const handleNotificationPermission = async () => {
     await requestNotificationPermission(true);
     window.location.reload();
   };
+
+  let notificationPermission: NotificationPermission;
+
+  onMount(async () => {
+    notificationPermission = await requestNotificationPermission();
+  });
 </script>
 
 <button on:click={handleNotificationPermission}>Request Notification Permission</button>
 
-<p>Notification permission: {requestNotificationPermission()}</p>
+<p>Notification permission: {notificationPermission}</p>
